95fead7ca359597397e60ed48a43239e02be3f35
jing kwan jung <jingkwanjung@jingui-MacBookAir.local>
2023-02-26 15:47:31 +0900

    initial-commit

---
+++ src/main/java/com/user/integratedusermanagementapi/IntegratedUserManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.user.integratedusermanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegratedUserManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegratedUserManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/user/integratedusermanagementapi/IntegratedUserManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.user.integratedusermanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegratedUserManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
