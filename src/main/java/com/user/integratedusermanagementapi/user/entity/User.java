260c2a1d2315f310a249c62043a22d6d9e27fb34
정진관 <dingwan0331@gmail.com>
2023-03-12 18:27:54 +0900

    feat: user 엔티티 작성

---
+++ src/main/java/com/user/integratedusermanagementapi/user/entity/User.java
@@ -0,0 +1,34 @@
+package com.user.integratedusermanagementapi.user.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Setter
+@Getter
+@Entity
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "external_key", nullable = false, unique=true)
+    private String external_key;
+
+    @Column(name = "user_name", nullable = true, length = 20,unique=true)
+    private String userName;
+
+    @Column(name = "nick_name", nullable = true, length = 20, unique=true)
+    private String nickName;
+
+    @Column(name = "mobile_phone_number", nullable = false, length = 20)
+    private String mobilePhoneNumber;
+
+    @Column(name = "passwrod", nullable = true, length = 60)
+    private Byte password;
+}
