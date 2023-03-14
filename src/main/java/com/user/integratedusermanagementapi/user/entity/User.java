a2cd83979a2f49f1d109adc5d8a8b1efeaa45142
jing kwan jung <jingkwanjung@jingui-MacBookAir.local>
2023-03-14 19:21:52 +0900

    refactor: UserEntity 상속받아서 구현하는것으로 리팩토링

--- src/main/java/com/user/integratedusermanagementapi/user/entity/User.java
+++ src/main/java/com/user/integratedusermanagementapi/user/entity/User.java
@@ -1,29 +1,23 @@
 package com.user.integratedusermanagementapi.user.entity;
 
-import jakarta.persistence.*;
+import com.user.integratedusermanagementapi.base.entity.BaseEntityWithExternalKey;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
 
 @Setter
 @Getter
 @Entity
 @NoArgsConstructor
 @Table(name = "users")
-public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-
-    @GeneratedValue(strategy = GenerationType.UUID)
-    @Column(name = "external_key", nullable = false, unique=true)
-    private String external_key;
-
+public class User extends BaseEntityWithExternalKey {
     @Column(name = "username", nullable = true, length = 20,unique=true)
     private String username;
 
-    @Column(name = "nick_name", nullable = true, length = 20, unique=true)
+    @Column(name = "nick_name", nullable = false, length = 20, unique=true)
     private String nickName;
 
     @Column(name = "mobile_phone_number", nullable = false, length = 20)
