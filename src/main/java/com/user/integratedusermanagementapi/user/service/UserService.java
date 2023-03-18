70da5a90cb495e595f05a0bef384bc38423a758f
정진관 <dingwan0331@gmail.com>
2023-03-18 17:24:06 +0900

    refactor: userService Autowired 추가

--- src/main/java/com/user/integratedusermanagementapi/user/service/UserService.java
+++ src/main/java/com/user/integratedusermanagementapi/user/service/UserService.java
@@ -2,9 +2,7 @@ package com.user.integratedusermanagementapi.user.service;
 
 import com.user.integratedusermanagementapi.user.controller.dto.request.PostUserRequestDTO;
 import com.user.integratedusermanagementapi.user.entity.User;
-import org.springframework.stereotype.Service;
 
-@Service
 public interface UserService {
     User createUser(PostUserRequestDTO postUserRequestDto);
 }
--- src/main/java/com/user/integratedusermanagementapi/user/service/UserServiceImpl.java
+++ src/main/java/com/user/integratedusermanagementapi/user/service/UserServiceImpl.java
@@ -3,12 +3,20 @@ package com.user.integratedusermanagementapi.user.service;
 import com.user.integratedusermanagementapi.user.controller.dto.request.PostUserRequestDTO;
 import com.user.integratedusermanagementapi.user.entity.User;
 import com.user.integratedusermanagementapi.user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl implements UserService{
     UserRepository userRepository;
-    User user;
+
+    @Autowired
+    public UserServiceImpl( UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
     @Override
     public User createUser(PostUserRequestDTO postUserRequestDto) {
+        User user = new User();
         user.setUsername(postUserRequestDto.getUsername());
         user.setPassword(postUserRequestDto.getPassword().getBytes());
         user.setNickName(postUserRequestDto.getNickName());
